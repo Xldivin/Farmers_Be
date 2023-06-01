af7f5a481069159a68bcbdb30615eebca7080489
axel <xldivin@gmail.com>
2023-06-01 07:43:24 +0200

    added all files for the farmers_be

---
+++ src/controllers/userController.ts
@@ -0,0 +1,61 @@
+import { Request, Response } from 'express';
+import { dbConnect } from '../db/index.js';
+import { IUser } from '../models/user.js';
+import { ObjectId } from 'mongodb';
+
+export const createUser = () => {
+    return async (req: Request, res: Response) => {
+      const db = await dbConnect();
+      const { firstName, lastName, email, password, role } = req.body;
+  
+      const newUser: IUser = {
+        firstName,
+        lastName,
+        email,
+        password,
+        role
+      };
+  
+      const users = await db?.users.insertOne(newUser);
+      if (!users) {
+        console.log("request failed");
+        const errorResponse = { error: 'request failed' };
+        res.status(400).json(errorResponse);
+        return;
+      }
+  
+      const response = { data: newUser, status: 201 };
+      res.status(response.status).json(response);
+    };
+};
+
+export const login = () => {
+    return async (req: Request, res: Response) => {
+      const db = await dbConnect();
+      const { email, password } = req.body;
+      const id = req.params.id;
+  
+      const user = await db?.users.findOne({ _id: new ObjectId(id)});
+  
+      if (!user) {
+        const errorResponse = { error: 'Invalid email or password' };
+        res.status(401).json(errorResponse);
+        return;
+      }
+      if (password !== user.password) {
+        const errorResponse = { error: 'Invalid email or password' };
+        res.status(401).json(errorResponse);
+        return;
+      }
+
+      if (email !== user.email) {
+        const errorResponse = { error: 'Invalid email or password' };
+        res.status(401).json(errorResponse);
+        return;
+      }
+  
+      const response = { user, status: 200 };
+      res.status(response.status).json(response);
+    };
+};
+  
